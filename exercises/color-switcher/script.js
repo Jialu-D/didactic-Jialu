d047f6ba1325588d1df6e5ff7b40d780c6fc4ff5
Jialu-D <jld2236@columbia.edu>
2021-11-16 21:02:17 -0500

    Update script.js

--- exercises/color-switcher/script.js
+++ exercises/color-switcher/script.js
@@ -1,9 +1,17 @@
 let yellowCircle=document.getElementById("yellowButton");
+let blueCircle=document.getElementById("blueButton");
 
 yellowCircle.addEventListener("click",turnPageYellow);
+blueCircle.addEventListener("click",turnPageBlue);
 
 function turnPageYellow(){
 console.log("turn me yellow");
 let body=document.querySelector("body");
 body.style.backgroundColor="yellow";
 }
+
+function turnPageBlue(){
+    console.log("turn me blue");
+    let body=document.querySelector("body");
+    body.style.backgroundColor="blue";
+    }
